dca536ac198f212d457106871df7fd31016263e1
maoqxm <maoqxm@gmail.com>
2020-03-05 00:00:30 +0800

    :sparkles: add money account card in dashboard

--- src/app/views/dashboard/index.tsx
+++ src/app/views/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BudgetCard } from "./budget-card";
+import { AccountsCard } from "./accounts";
 
 export const Dashboard: React.FC = () => {
   return (
     <div className="dashboard">
       <BudgetCard />
+      <AccountsCard />
     </div>
   );
 };
