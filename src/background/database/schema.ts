65dc3a2531a83552eca215afa701c925f12bd6b8
maoqxm <maoqxm@gmail.com>
2020-03-11 23:54:57 +0800

    :construction: 增加相关类型定义

--- src/background/database/schema.ts
+++ src/background/database/schema.ts
@@ -15,8 +15,27 @@ export interface BillItem<IdType> {
   reimbursable: boolean;
 }
 
+// 客户端发送的创建模型
 export type BillItemCreate = Omit<BillItem<string>, "_id">;
 
+// 数据库里存的模型
 export type BillItemDB = BillItem<ObjectId>;
 
+// 客户端使用的模型
 export type BillItemApp = BillItem<string>;
+
+// 个人配置
+export interface Preference {
+  // 月度预算
+  monthlyBudget: number;
+}
+
+// 账目分类
+export interface BillCategory {
+  // 标识
+  _id: string;
+  // 类型：0（支出）| 1（收入）
+  type: 0 | 1;
+  // 类型的描述文本
+  text: string;
+}
